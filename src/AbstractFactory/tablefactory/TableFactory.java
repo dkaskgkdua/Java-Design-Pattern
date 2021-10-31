b29ea8fc80ab9e39ee989e2908d72334346ed237
SongMinJun <dkaskgkdua@gmail.com>
2021-11-01 00:48:47 +0900

    Abstract Factory 패턴 https://song8420.tistory.com/391

---
+++ src/AbstractFactory/tablefactory/TableFactory.java
@@ -0,0 +1,18 @@
+package AbstractFactory.tablefactory;
+
+import AbstractFactory.factory.Factory;
+import AbstractFactory.factory.Link;
+import AbstractFactory.factory.Page;
+import AbstractFactory.factory.Tray;
+
+public class TableFactory extends Factory {
+    public Link createLink(String caption, String url) {
+        return new TableLink(caption, url);
+    }
+    public Tray createTray(String caption) {
+        return new TableTray(caption);
+    }
+    public Page createPage(String title, String author) {
+        return new TablePage(title, author);
+    }
+}
